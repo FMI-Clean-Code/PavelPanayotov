3468380d76cdbf9ff188b7a1de104ff1edaf5c60
Halmi.Hodzhov <halmihodzhov97@gmail.com>
2018-03-28 11:53:57 +0300

    init matrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
 public class Main {
+   private int sizeOfMatrix;
+    int [][] matrix;
+
+    public Main(int sizeOfMatrix) {
+        this.sizeOfMatrix = sizeOfMatrix;
+        matrix = new int[][]{
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}
+
+
+        };
+    }
 
     public static void main(String[] args) {
-        System.out.println("Asdasdasdasd");
+
+        initializeMatrix();
+
+    }
+
+    private static void initializeMatrix() {
+
+
+
+
+
+
     }
 }
