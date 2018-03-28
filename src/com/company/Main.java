59ae9504c976365790becf4123dbb59ae77c43ab
Halmi.Hodzhov <halmihodzhov97@gmail.com>
2018-03-28 12:18:20 +0300

    init matrix impl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-   private int sizeOfMatrix;
-    int [][] matrix;
+    private int sizeOfMatrix;
+    int[][] matrix;
+
 
     public Main(int sizeOfMatrix) {
-        matrix = new int[][]{
-                {1, 2, 3, 4, 5},
-                {6, 7, 8, 9, 10},
-                {11, 12, 13, 14, 15},
-                {16, 17, 18, 19, 20},
-                {21, 22, 23, 24, 25}
+        this.sizeOfMatrix = sizeOfMatrix;
 
-
-        };
-    }
-
-    public static void main(String[] args) {
-
-        initializeMatrix();
-
+        matrix = new int[5][5];
+        int counter = 0;
+        for(int i=0 ;i< sizeOfMatrix; i++){
+            for(int j=0; j<sizeOfMatrix; j++){
+                matrix[i][j] = ++counter;
+            }
+        }
     }
 
     private static void initializeMatrix() {
-
-
-
-
-
-
+        Main m = new Main(5);
+        m.printMatrix();
     }
+
+   public void printMatrix(){
+       for(int i=0 ;i< sizeOfMatrix; i++){
+           for(int j=0; j<sizeOfMatrix; j++){
+               System.out.print(matrix[i][j] + " ");
+           }
+           System.out.println();
+       }
+   }
+
+    public static void main(String[] args) {
+        initializeMatrix();
+    }
+
 }
