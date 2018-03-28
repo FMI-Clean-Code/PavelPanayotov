e085e9d85567c8ac404f10702ef1ee3c759275dc
Pavel Panayotov <pavel.panayotow@gmail.com>
2018-03-28 12:03:07 +0300

    renamed class

---
+++ src/com/company/FibChecker.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class FibChecker {
+    ArrayList<Integer> fibonaccis;
+    FibChecker(){
+        fibonaccis=new ArrayList<>();
+    }
+}
